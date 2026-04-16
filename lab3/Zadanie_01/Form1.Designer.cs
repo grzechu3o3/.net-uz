b8bdea79e9b542b33ad7af34e3e3ba1c5f3423a0
grzechu3o3 <jgrzesik7@icloud.com>
2026-04-16 15:49:09 +0200

    zadanie 1

---
+++ lab3/Zadanie_01/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Zadanie_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.compareBtn = new System.Windows.Forms.Button();
+            this.days = new System.Windows.Forms.Label();
+            this.hours = new System.Windows.Forms.Label();
+            this.secs = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.secs);
+            this.groupBox1.Controls.Add(this.hours);
+            this.groupBox1.Controls.Add(this.days);
+            this.groupBox1.Controls.Add(this.compareBtn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Porównywanie dat";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data od:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(63, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(63, 45);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data do:";
+            // 
+            // compareBtn
+            // 
+            this.compareBtn.Location = new System.Drawing.Point(207, 19);
+            this.compareBtn.Name = "compareBtn";
+            this.compareBtn.Size = new System.Drawing.Size(85, 46);
+            this.compareBtn.TabIndex = 4;
+            this.compareBtn.Text = "Porównaj";
+            this.compareBtn.UseVisualStyleBackColor = true;
+            this.compareBtn.Click += new System.EventHandler(this.compareBtn_Click);
+            // 
+            // days
+            // 
+            this.days.AutoSize = true;
+            this.days.Location = new System.Drawing.Point(9, 91);
+            this.days.Name = "days";
+            this.days.Size = new System.Drawing.Size(95, 13);
+            this.days.TabIndex = 5;
+            this.days.Text = "Różnica w dniach:";
+            // 
+            // hours
+            // 
+            this.hours.AutoSize = true;
+            this.hours.Location = new System.Drawing.Point(9, 112);
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(115, 13);
+            this.hours.TabIndex = 6;
+            this.hours.Text = "Różnica w godzinach: ";
+            // 
+            // secs
+            // 
+            this.secs.AutoSize = true;
+            this.secs.Location = new System.Drawing.Point(9, 134);
+            this.secs.Name = "secs";
+            this.secs.Size = new System.Drawing.Size(119, 13);
+            this.secs.TabIndex = 7;
+            this.secs.Text = "Różnica w sekundach: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 444);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button compareBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label secs;
+        private System.Windows.Forms.Label hours;
+        private System.Windows.Forms.Label days;
+    }
+}
+
---
+++ lab3/Zadanie_01/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_01
+{
+    public partial class Form1 : Form
+    {
+        private DateTime date1;
+        private DateTime date2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void compareBtn_Click(object sender, EventArgs e)
+        {
+            this.date1 = dateTimePicker1.Value;
+            this.date2 = dateTimePicker2.Value;
+
+            TimeSpan result = date1 - date2;
+            double resultdays = Math.Round(Math.Abs(result.TotalDays),2);
+            double resulthours = Math.Round(Math.Abs(result.TotalHours), 2);
+            double resultsecs = Math.Round(Math.Abs(result.TotalSeconds), 2);
+
+            days.Text = "Różnica w dniach: " + resultdays.ToString();
+            hours.Text = "Różnica w godzinach: " + resulthours.ToString();
+            secs.Text = "Różnica w sekundach: " + resultsecs.ToString();
+        }
+    }
+}
