ffacd44b45cfe4a5466cd6b379bf36fc57684b3f
grzechu3o3 <jgrzesik7@icloud.com>
2026-04-16 10:30:50 +0200

    Zadanie 5 i 7

---
+++ Zadanie_05/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Runtime.InteropServices.ObjectiveC;
+
+class Program
+{
+    static int globalMax = int.MinValue;
+    static object lockObj = new Object();
+
+    static void Main()
+    {
+        int[] dane = Generuj(10_000_000);
+
+        int liczbaWatkow = 8;
+        Thread[] watki = new Thread[liczbaWatkow];
+
+        int chunkSize = dane.Length / liczbaWatkow;
+
+        for(int i = 0; i < liczbaWatkow; i++)
+        {
+            int start = i * chunkSize;
+            int end = (i == liczbaWatkow - 1) ? dane.Length : start + chunkSize;
+
+            watki[i] = new Thread(() => SzukajMax(dane, start, end));
+            watki[i].Start();
+        }
+        foreach (var w in watki) w.Join();
+
+        Console.WriteLine($"Max = {globalMax}");
+    }
+
+    static void SzukajMax(int[] dane, int start, int end)
+    {
+        int localMax = int.MinValue;
+        
+        for(int i = start; i<end; i++)
+        {
+            if (dane[i] > localMax) localMax = dane[i];
+        }
+        lock (lockObj)
+        {
+            if (localMax > globalMax) globalMax = localMax;
+        }
+    }
+    
+    static int[] Generuj(int size)
+    {
+        Random rand = new Random();
+        int[] dane = new int[size];
+        for(int i=0;i<size;i++)
+        {
+            dane[i] = rand.Next();
+        }
+
+        return dane;
+    }
+}
---
+++ Zadanie_07/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+
+public class Zadanie07_Enumerator : IEnumerator<int>
+{
+    private int[] _data;
+    private int _index = -1;
+
+    public Zadanie07_Enumerator(int[] data)
+    {
+        _data = data;
+    }
+    public int Current => _data[_index];
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    { 
+    }
+
+    public bool MoveNext()
+    {
+        _index++;
+        return _index < _data.Length;
+    }
+
+    public void Reset()
+    {
+        _index = -1;
+    }
+}
+class Zadanie07_Enumerable : IEnumerable<int>
+{
+    private int[] _data;
+    public Zadanie07_Enumerable(int[] data)
+    {
+        _data = data;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        return new Zadanie07_Enumerator(_data);
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var kolekcja = new Zadanie07_Enumerable([1,2,3,6,7]);
+
+        foreach(int liczba in kolekcja)
+        {
+            Console.WriteLine(liczba);
+        }
+    }
+}
